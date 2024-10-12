5e81bd4879582d0ddb3e9861ea53603f1e057dfd
Lim Jun Kee <101441406+Lim1802@users.noreply.github.com>
2024-10-13 02:25:45 +0800

    Final version

---
+++ map.js
@@ -0,0 +1,12 @@
+
+
+var specVis1 = "map.json";
+var specVis2 = "scatterPlot.json";
+var specVis3 = "smallMultiples.json";
+var specVis4 = "radialChart.json";
+var specVis5 = "waterFall.json";
+vegaEmbed('#vis',specVis1);
+vegaEmbed('#vis2',specVis2);
+vegaEmbed('#vis3',specVis3);
+vegaEmbed('#vis4',specVis4);
+vegaEmbed('#vis5',specVis5);
